7fbddf3c89d00abbf8e3325169799bbf2ed51ae0
Arielsonsantana12 <136532384+Arielsonsantana12@users.noreply.github.com>
2026-04-21 16:17:27 -0300

    Atualização

--- Assets/Scripts/BossScript.cs
+++ Assets/Scripts/BossScript.cs
@@ -1,22 +1,27 @@
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 
 public class BossScript : MonoBehaviour
 {
     public LayerMask camdachao;
-    
-    public float velocidadeMoviumento = 5f;
-    
+
+    public float velocidadeMovimento = 5f;
+    public float distanciaPatrulha = 3f; // O quanto ele se afasta do centro
+
+    private float posicaoXInicial;
+
     void Start()
     {
-        
+        // Salva a posição X de onde o Boss começou
+        posicaoXInicial = transform.position.x;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.right * velocidadeMoviumento * Time.deltaTime;  
-    }
+        // O Seno cria um valor que vai de -1 a 1 repetidamente
+        // Multiplicamos pela distanciaPatrulha para definir o alcance
+        float deslocamentoX = Mathf.Sin(Time.time * velocidadeMovimento) * distanciaPatrulha;
 
-    
+        // Atualiza a posição apenas no eixo X, mantendo o Y e Z originais
+        transform.position = new Vector3(posicaoXInicial + deslocamentoX, transform.position.y, transform.position.z);
+    }
 }
